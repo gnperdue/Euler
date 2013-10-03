071c4d2814f528c257fda55c26eef01860a6bf6f
Gabriel Perdue <perdue@fnal.gov>
2013-10-03 08:20:15 -0500

    Wasn't actually answering the question. Now I am.

--- Code/Erlang/problem014/euler014.erl
+++ Code/Erlang/problem014/euler014.erl
@@ -1,10 +1,11 @@
 -module(euler014).
--export([collatzl/1,longest_collatz1/1,longest_collatz2/1]).
+-export([collatzl/1,longest_collatz1/1,longest_collatz2/1,longest_collatz3/1]).
+
+%% longest_collatz1 and longest_collatz2 do the same thing...
 
 longest_collatz1(Max) ->
   lists:foldl( 
     fun(X, Longest) -> 
-        io:format("~w, ~w~n",[X,Longest]),
         if X > Longest ->
             X;
           X =< Longest ->
@@ -19,7 +20,6 @@ longest_collatz2(Max) ->
   lists:foldl( 
     fun(X, Longest) ->
         L = collatzl(X), 
-        io:format("~w, ~w~n",[L,Longest]),
         if L > Longest ->
             L;
           L =< Longest ->
@@ -27,6 +27,20 @@ longest_collatz2(Max) ->
         end
     end,
     0, 
+    lists:seq(1,Max) 
+  ).
+
+longest_collatz3(Max) -> 
+  lists:foldl( 
+    fun(X, {N,Longest}) ->
+        L = collatzl(X), 
+        if L > Longest ->
+            {X,L};
+          L =< Longest ->
+            {N,Longest}
+        end
+    end,
+    {0,0}, 
     lists:seq(1,Max) 
   ).
 
