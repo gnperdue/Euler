d83c846ffea88c22df5ef2a7beea2f9236ff4091
Gabriel Perdue <perdue@fnal.gov>
2013-10-03 08:11:01 -0500

    Debug version of solution to problem 14.

--- Code/Erlang/problem014/euler014.erl
+++ Code/Erlang/problem014/euler014.erl
@@ -1,16 +1,42 @@
 -module(euler014).
--export([collatzl/1]).
+-export([collatzl/1,longest_collatz1/1,longest_collatz2/1]).
+
+longest_collatz1(Max) ->
+  lists:foldl( 
+    fun(X, Longest) -> 
+        io:format("~w, ~w~n",[X,Longest]),
+        if X > Longest ->
+            X;
+          X =< Longest ->
+            Longest
+        end
+    end, 
+    0, 
+    [collatzl(N) || N <- lists:seq(1,Max)]).
+
+
+longest_collatz2(Max) -> 
+  lists:foldl( 
+    fun(X, Longest) ->
+        L = collatzl(X), 
+        io:format("~w, ~w~n",[L,Longest]),
+        if L > Longest ->
+            L;
+          L =< Longest ->
+            Longest
+        end
+    end,
+    0, 
+    lists:seq(1,Max) 
+  ).
 
 collatzl(Start) ->
   collatzl(Start,1).
 
 collatzl(Number,Length) when Number =:= 1 -> 
-  io:format("One Clause: Number = ~w, Length = ~w.~n",[Number,Length]),
   Length;
 collatzl(Number,Length) when Number rem 2 =:= 0 -> 
-  io:format("Even Clause: Number = ~w, Length = ~w.~n",[Number,Length]),
   collatzl( Number div 2, Length+1 );
 collatzl(Number,Length) -> 
-  io:format("General Clause: Number = ~w, Length = ~w.~n",[Number,Length]),
   collatzl( 3*Number + 1, Length+1 ).
 
