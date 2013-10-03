854f56ebe7bcf535562815396142ea9d56b92f90
Gabriel Perdue <perdue@fnal.gov>
2013-10-03 08:23:36 -0500

    Cleanup scratch functions.

--- Code/Erlang/problem014/euler014.erl
+++ Code/Erlang/problem014/euler014.erl
@@ -1,36 +1,7 @@
 -module(euler014).
--export([collatzl/1,longest_collatz1/1,longest_collatz2/1,longest_collatz3/1]).
+-export([collatzl/1,longest_collatz/1]).
 
-%% longest_collatz1 and longest_collatz2 do the same thing...
-
-longest_collatz1(Max) ->
-  lists:foldl( 
-    fun(X, Longest) -> 
-        if X > Longest ->
-            X;
-          X =< Longest ->
-            Longest
-        end
-    end, 
-    0, 
-    [collatzl(N) || N <- lists:seq(1,Max)]).
-
-
-longest_collatz2(Max) -> 
-  lists:foldl( 
-    fun(X, Longest) ->
-        L = collatzl(X), 
-        if L > Longest ->
-            L;
-          L =< Longest ->
-            Longest
-        end
-    end,
-    0, 
-    lists:seq(1,Max) 
-  ).
-
-longest_collatz3(Max) -> 
+longest_collatz(Max) -> 
   lists:foldl( 
     fun(X, {N,Longest}) ->
         L = collatzl(X), 
