93de6e5a4ece179153d718c8e21243b1b6dd6775
Gabriel Perdue <gnperdue@me.com>
2013-10-11 09:53:43 -0500

    Fix script for new location.

--- Code/setup_euler.sh
+++ Code/setup_euler.sh
@@ -4,9 +4,9 @@ ARCHIT=`uname`
 
 if [ $ARCHIT = 'Darwin' ]
 then
-  export DYLD_LIBRARY_PATH=`pwd`/lib:$DYLD_LIBRARY_PATH
+  export DYLD_LIBRARY_PATH=`pwd`/CPP/lib:$DYLD_LIBRARY_PATH
 else
-  export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=`pwd`/CPP/lib:$LD_LIBRARY_PATH
 fi
 
 unset ARCHIT
