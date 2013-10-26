37aab4df259dca4bcb85f19c20487ca035889bbd
Gabriel Perdue <gnperdue@me.com>
2013-10-25 19:27:29 -0500

    Clean up the answer module a bit by taking out the functions that built up to the answer, but save those build-up functions in another module for reference.

---
+++ Code/Erlang/problem065/continued_fractions.erl
@@ -0,0 +1,95 @@
+-module(continued_fractions).
+-export([
+    eval_frac/1
+    ,frac_div/2
+    ,frac_add/2
+    ,cont_frac/2
+    ,cont_frac_l/2
+  ]
+).
+
+%% @doc Compute a continuted fraction for N convergences with an
+%% array-numerator and report the answer as a fraction.
+cont_frac_l(Nconvergents, {Start,DenomList}) ->
+  CyclePos = 1,
+  io:format("~w + ~n",[Start]),
+  frac_add(
+    {Start,1}, 
+    cont_denom_l(1, Nconvergents, {1,DenomList,CyclePos})
+  ).
+
+%% @doc Compute the numerator-array continued fractioin recursively.
+cont_denom_l(Iter, Nconvergents, {Numer, DenomList,CyclePos}) ->
+  Nitems = length(DenomList),
+  if
+    CyclePos >  Nitems -> 
+      Index = CyclePos - Nitems;
+    CyclePos =< Nitems -> 
+      Index = CyclePos
+  end,
+  Denom  = lists:nth(Index,DenomList),
+  if 
+    Iter >= Nconvergents -> 
+      io:format(" ~w/~w~n",[Numer,Denom]),
+      frac_div({Numer,1},{Denom,1});
+    Iter <  Nconvergents -> 
+      io:format("~w / ~w + ~n",[Numer,Denom]),
+      frac_div(
+        {Numer,1},
+        frac_add(
+          {Denom,1}, 
+          cont_denom_l(Iter+1, Nconvergents, {Numer, DenomList, Index+1})
+        )
+      )
+  end.
+
+
+%% @doc Compute a simple continuted fraction for N convergences and 
+%% report the answer as a fraction.
+cont_frac(Nconvergents, {Start,Denom}) ->
+  frac_add(
+    {Start,1}, 
+    frac_div(
+      {1,1}, 
+      cont_denom(1, Nconvergents, {1,Denom})
+    )
+  ).
+
+%% @doc Compute the continued fractioin recursively.
+cont_denom(Iter, Nconvergents, {Numer, Denom}) ->
+  if 
+    Iter >= Nconvergents -> {Denom,1};
+    Iter <  Nconvergents -> 
+      frac_add( 
+        {Denom,1}, 
+        frac_div({Numer,1}, cont_denom(Iter+1, Nconvergents, {Numer, Denom})) 
+      )
+  end.
+
+%% @doc Turn a whole fraction into a floating point number.
+eval_frac({Numer,Denom}) -> Numer / Denom.
+
+%% @doc Do ``fractional addition'' where the tuple  {A,B} represents
+%% the {numerator, denominator}:  A/B + C/D = (A*D + C*B)/(B*D)
+frac_add({A,B},{C,D}) ->
+  frac_reduce({A*D + B*C, B*D}).
+
+%% @doc Do ``fractional division'' where the tuple {A,B} represents
+%% the {numerator, denominator}:  A/B / C/D = (A*D)/(B*C)
+frac_div({A,B},{C,D}) ->
+  frac_reduce({A*D, B*C}).
+
+%% @doc Reduce the fraction where the tuple {A,B} represents
+%% the {numerator, denominator}.
+frac_reduce({X,Y}) ->
+  Gcd = dijkstra_gcd({X,Y}),
+  {X div Gcd, Y div Gcd}.
+
+%% @doc Compute the greatest common denominator of two numbers.
+dijkstra_gcd({M,N}) ->
+  if
+    M == N -> M;
+    M > N  -> dijkstra_gcd({M-N,N});
+    M < N  -> dijkstra_gcd({M,N-M})
+  end.
+
--- Code/Erlang/problem065/euler065.erl
+++ Code/Erlang/problem065/euler065.erl
@@ -1,11 +1,7 @@
 -module(euler065).
 -export([
-    eval_frac/1
-    ,frac_div/2
+    frac_div/2
     ,frac_add/2
-    ,cont_frac/2
-    ,cont_denom/3
-    ,cont_frac_l/2
     ,cont_frac_e/1
     ,cont_frac_e/2
     ,euler/1
@@ -65,67 +61,6 @@ cont_denom_e(Iter, Nconvergents, {Numer, DenomList,CyclePos}) ->
         )
       )
   end.
-
-%% @doc Compute a continuted fraction for N convergences with an
-%% array-numerator and report the answer as a fraction.
-cont_frac_l(Nconvergents, {Start,DenomList}) ->
-  CyclePos = 1,
-  io:format("~w + ~n",[Start]),
-  frac_add(
-    {Start,1}, 
-    cont_denom_l(1, Nconvergents, {1,DenomList,CyclePos})
-  ).
-
-%% @doc Compute the numerator-array continued fractioin recursively.
-cont_denom_l(Iter, Nconvergents, {Numer, DenomList,CyclePos}) ->
-  Nitems = length(DenomList),
-  if
-    CyclePos >  Nitems -> 
-      Index = CyclePos - Nitems;
-    CyclePos =< Nitems -> 
-      Index = CyclePos
-  end,
-  Denom  = lists:nth(Index,DenomList),
-  if 
-    Iter >= Nconvergents -> 
-      io:format(" ~w/~w~n",[Numer,Denom]),
-      frac_div({Numer,1},{Denom,1});
-    Iter <  Nconvergents -> 
-      io:format("~w / ~w + ~n",[Numer,Denom]),
-      frac_div(
-        {Numer,1},
-        frac_add(
-          {Denom,1}, 
-          cont_denom_l(Iter+1, Nconvergents, {Numer, DenomList, Index+1})
-        )
-      )
-  end.
-
-
-%% @doc Compute a simple continuted fraction for N convergences and 
-%% report the answer as a fraction.
-cont_frac(Nconvergents, {Start,Denom}) ->
-  frac_add(
-    {Start,1}, 
-    frac_div(
-      {1,1}, 
-      cont_denom(1, Nconvergents, {1,Denom})
-    )
-  ).
-
-%% @doc Compute the continued fractioin recursively.
-cont_denom(Iter, Nconvergents, {Numer, Denom}) ->
-  if 
-    Iter >= Nconvergents -> {Denom,1};
-    Iter <  Nconvergents -> 
-      frac_add( 
-        {Denom,1}, 
-        frac_div({Numer,1}, cont_denom(Iter+1, Nconvergents, {Numer, Denom})) 
-      )
-  end.
-
-%% @doc Turn a whole fraction into a floating point number.
-eval_frac({Numer,Denom}) -> Numer / Denom.
 
 %% @doc Do ``fractional addition'' where the tuple  {A,B} represents
 %% the {numerator, denominator}:  A/B + C/D = (A*D + C*B)/(B*D)
