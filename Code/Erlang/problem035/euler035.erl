fc098be55aef147cfa3da540d4f60da51da57888
Gabriel Perdue <gnperdue@me.com>
2013-10-27 10:10:00 -0500

    Make a list of the cyclic permutations.

--- Code/Erlang/problem035/euler035.erl
+++ Code/Erlang/problem035/euler035.erl
@@ -1,17 +1,31 @@
 -module(euler035).
 -export([
-    make_rotations/1
-    ,make_cyclic_permutation/1
-    ,permute_digits/1
-    ,permute/1
-    ,highest_power/1
+    permute/1
+    ,ndigits/1
+    ,list_of_cyclic_perms/1
   ]
 ).
 
-% permute(N) ->
-%   X = N rem 10, _Y = (N - X) div 10,
-%   X.
+list_of_cyclic_perms(N) ->
+  Ndig = ndigits(N),
+  List = [N],
+  list_of_cyclic_perms(N,List,1,Ndig).
+list_of_cyclic_perms(_N, List, Iter, MaxIter) when Iter >= MaxIter -> 
+  List;
+list_of_cyclic_perms(N, List, Iter, MaxIter) ->
+  NewN = permute(N),
+  NewList = [NewN | List],
+  list_of_cyclic_perms(NewN, NewList, Iter+1, MaxIter).
 
+ndigits(N) ->
+  highest_power(N) + 1.
+
+permute(N) ->
+  H = math:pow(10,highest_power(N)),
+  X = N rem 10, Y = (N - X) div 10,
+  trunc(X*H + Y).
+
+%% @doc 10^{what} is the highest power?
 highest_power(N) ->
   highest_power(N,0).
 highest_power(0,Acc) -> Acc;
@@ -22,33 +36,4 @@ highest_power(N,Acc) ->
     Tens /= 0 -> highest_power(Tens,Acc+1)
   end.
 
-permute_digits(N) ->
-  trunc( revert_list_to_number( make_cyclic_permutation( make_digit_list(N) ) ) ).
-
-make_rotations(0) -> [];
-make_rotations(N) when N < 0 -> make_rotations(-N);
-make_rotations(N) ->
-  make_digit_list(N).
-
-make_digit_list(N) ->
-  make_digit_list(N,[]).
-make_digit_list(0,List) -> List;
-make_digit_list(N,List) ->
-  X = N rem 10, Y = (N - X) div 10,
-  make_digit_list(Y,[X | List]).
-
-make_cyclic_permutation([]) -> [];
-make_cyclic_permutation(List) -> 
-  Length = length(List),
-  {Lead,[H|_T]} = lists:split(Length-1,List),
-  % [H|_T] = Last,
-  [H|Lead].
-
-revert_list_to_number(List) ->
-  revert_list_to_number(List,0).
-revert_list_to_number([],N) -> N;
-revert_list_to_number(List,N) ->
-  Power = math:pow(10,length(List)-1),
-  [H|T] = List,
-  revert_list_to_number(T,N+Power*H).
 
