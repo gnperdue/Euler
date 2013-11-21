7b423c5ccf827e5878893ae81765d7f9222a2590
Gabriel Perdue <perdue@fnal.gov>
2013-11-21 09:21:07 -0600

    Work on problem 10 in C. This takes long enough to be worth testing with some parallel approaches.

---
+++ Code/C/problem010/euler010.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int is_prime(unsigned long num);
+
+int main(int argc, const char *argv[])
+{
+	unsigned long the_number = 2000000L;
+	if (argc > 1) {
+		the_number = strtol(argv[1], NULL, /*base*/10);
+	}
+	printf("The number is: %ld\n", the_number);
+
+	unsigned long counter = 3L;
+	unsigned long sum = 2L;
+
+	while (counter < the_number) {
+		if (1L == is_prime(counter)) {
+			sum += counter;
+		}
+		counter = counter + 2L;
+	}
+	printf(" The sum of the primes is: %ld\n", sum);
+
+	return 0;
+}
+
+int is_prime(unsigned long num)
+{
+	if (num <  2L) return 0;
+	if (num == 2L) return 1;
+	if (num == 3L) return 1;
+	unsigned long divisor = num - 1L;
+	while (divisor > 1L) {
+		if (num % divisor == 0) return 0;
+		divisor--;
+	}	
+	return 1;
+}
