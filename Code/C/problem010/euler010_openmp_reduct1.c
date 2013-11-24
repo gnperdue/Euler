59a46b1496e8635a661cedc1d628a4c6ac2cbed0
Gabriel Perdue <gnperdue@me.com>
2013-11-23 19:25:44 -0600

    Use OpenMP with a simple reduction.

---
+++ Code/C/problem010/euler010_openmp_reduct1.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+void usage(const char* prog_name);
+int is_prime(unsigned long num);
+unsigned long prime_sum(unsigned long max_num);
+
+int main(int argc, const char *argv[])
+{
+  if (argc < 2) usage(argv[0]);
+	unsigned long the_number = 2000000L;
+	printf("The number is: %ld\n", the_number);
+
+  int thread_count = 1;
+#ifdef _OPENMP
+  thread_count = strtol(argv[1], NULL, 10);
+#else
+  printf("OpenMP is not available. Thread count is 1!\n");
+#endif
+
+	unsigned long sum = 0L;
+
+#pragma omp parallel num_threads(thread_count) reduction(+: sum)
+	sum += prime_sum(the_number);
+
+  printf(" The sum of the primes is: %ld\n", sum);
+
+  return 0;
+}
+
+unsigned long prime_sum(unsigned long max_num)
+{
+#ifdef _OPENMP
+	int my_rank = omp_get_thread_num();
+	int thread_count = omp_get_num_threads();
+#else
+	int my_rank = 0;
+	int thread_count = 1;
+#endif
+
+  unsigned long section_size = max_num / thread_count;
+  unsigned long edge_piece = max_num - section_size*thread_count;
+	if (0 != edge_piece) {
+		fprintf(stderr, "The max number must be evenly divisible by the number of threads.\n");
+		exit(0);
+	}
+
+	unsigned long local_min = section_size * my_rank;
+	unsigned long local_max = local_min + section_size;
+
+	unsigned long counter = local_min;
+	unsigned long sum = 0L;
+	if (counter <= 2) {
+		sum = 2L;
+		counter = 3L;
+	}
+	else {
+		if (0 == counter % 2) counter++;
+	}
+
+  while (counter <= local_max) {
+		if (1L == is_prime(counter)) {
+			sum += counter;
+		}
+		counter = counter + 2L;
+	}
+	printf("The local sum in thread %d of %d is = %ld.\n", 
+			my_rank, thread_count, sum);
+
+	return sum;
+}
+
+
+int is_prime(unsigned long num)
+{
+	if (num <  2L) return 0;
+	if (num == 2L) return 1;
+	if (num == 3L) return 1;
+	unsigned long divisor = num - 1L;
+	while (divisor > 1L) {
+		if (num % divisor == 0) return 0;
+		divisor--;
+	}	
+	return 1;
+}
+
+void usage(const char* prog_name)
+{
+	printf("Usage: %s nthreads \n", prog_name);
+	exit(0);
+}
+
