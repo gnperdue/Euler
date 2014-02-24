d12bdcb064a06b13f1710f68c70bc641123f864c
Gabriel Perdue <gnperdue@me.com>
2014-02-23 20:42:43 -0600

    Do problem 3 in very naive Python, and set the ignore file to be blind to __pycache__.

---
+++ Code/Python/problem003/euler003.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import math
+
+theNumber = 600851475143
+
+
+def startingPoint(num):
+    start = math.floor(math.sqrt(num))
+    print("The starting point is {0}".format(start))
+    return start
+
+
+def isprime(num):
+    factor = num - 1
+    while (factor > 1):
+        if num % factor == 0:
+            return False
+        factor = factor - 1
+    return True
+
+
+def euler(num, test):
+    while test > 0:
+        if num % test == 0 and isprime(test):
+            print("The largest prime factor is {0}".format(test))
+            return test
+        test = test - 1
+
+
+if __name__ == '__main__':
+    euler(theNumber, startingPoint(theNumber))
