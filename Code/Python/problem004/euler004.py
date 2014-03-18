2628f5d057aef21722858b3196e9a0e55328d6f0
Gabriel Perdue <gnperdue@me.com>
2014-03-18 12:23:33 -0500

    Do problem 4 in Python.

---
+++ Code/Python/problem004/euler004.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+def is_palindromic(num):
+    if str(num) == str(num)[::-1]:
+        return True
+    return False
+
+
+def euler004():
+    max_palindrome = 0
+    for n1 in range(900, 1000):
+        for n2 in range(900, 1000):
+            n = n1 * n2
+            if is_palindromic(n):
+                if n > max_palindrome:
+                    max_palindrome = n
+    return max_palindrome
+
+
+if __name__ == '__main__':
+    answer = euler004()
+    print(answer)
+    # else we have been loaded as a module...
