963bd4b36d8b3f2a6805cae7d018d90b54a0d7c6
Gabriel Perdue <gnperdue@me.com>
2014-03-27 05:21:45 -0500

    Do problem 7 in Python.

---
+++ Code/Python/problem007/euler007.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import math
+
+
+def check_prime(number):
+    sqrt_num = math.sqrt(number)
+    float_num = float(number)
+    for i in xrange(2, int(sqrt_num) + 1):
+        if (float_num / i).is_integer():
+            return False
+    return True
+
+
+def get_nth_prime(n):
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    if n <= 10:
+        return primes[n - 1]
+    else:
+        count = 10
+        test = 30
+        while True:
+            if check_prime(test):
+                count += 1
+                if count == n:
+                    return test
+            test += 1
+
+
+if __name__ == '__main__':
+    print "The answer is %d" % get_nth_prime(10001)
