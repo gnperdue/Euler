cf8603e661bb537baff670e6b4cda30b672ba0f4
Gabriel Perdue <perdue@fnal.gov>
2014-04-14 08:15:16 -0500

    Do problem 6 in C++ using Intel Thread Building Blocks. It is non-deterministic!!! (Which I'm sure is my fault right now and not TBB's.)

---
+++ Code/CPP/problem006/euler006.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "tbb/parallel_for.h"
+#include "tbb/blocked_range.h"
+
+int sum_of_squares(int max) {
+  int sum {0};
+  tbb::parallel_for(
+			static_cast<int>(1), max+1,
+      [&](int i){
+      sum += i*i;
+      }
+      );
+  return sum;
+}
+
+int square_of_sum(int max) {
+  int sum {0};
+  tbb::parallel_for(
+			static_cast<int>(1), max+1,
+      [&](int i){
+      sum += i;
+      }
+      );
+  return sum * sum;
+}
+
+int main(int argc, char * argv[]) {
+
+  int max = 100;
+	int sq_of_sum = square_of_sum(max);
+	int sum_of_sq = sum_of_squares(max);
+	int euler = sq_of_sum - sum_of_sq;
+  std::cout << "The answer is " << euler << std::endl;
+
+  return 0;
+}
+
