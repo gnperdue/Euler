0f2e514e57bdc0aee914e62b35e2bf1eb8fb9db7
Gabriel Perdue <perdue@fnal.gov>
2014-04-14 08:03:35 -0500

    Be a bit smarter about setting up the environment.

--- Code/setup_euler.sh
+++ Code/setup_euler.sh
@@ -2,12 +2,16 @@
 
 ARCHIT=`uname`
 
+pushd $HOME/Projects/Euler/Code >& /dev/null
+
 if [ $ARCHIT = 'Darwin' ]
 then
   export DYLD_LIBRARY_PATH=`pwd`/CPP/lib:$DYLD_LIBRARY_PATH
 else
   export LD_LIBRARY_PATH=`pwd`/CPP/lib:$LD_LIBRARY_PATH
 fi
+
+popd >& /dev/null
 
 unset ARCHIT
 
