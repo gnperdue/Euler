04ae59949c60aa11cb687da89e9826fdf3b834b9
Gabriel Perdue <gnperdue@me.com>
2014-05-02 21:01:26 -0500

    Do problem 11 in sort of inelegant Python.

--- Code/Python/problem011/euler011.py
+++ Code/Python/problem011/euler011.py
@@ -18,49 +18,122 @@ def get_the_data():
 
 # test the vertical and horizontal first
 def scan(data_frame):
+    largest_product = np.max(np.array([get_vert(data_frame),
+                                       get_horiz(data_frame),
+                                       get_nw_to_se(data_frame),
+                                       get_se_to_nw(data_frame),
+                                       get_sw_to_ne(data_frame),
+                                       get_ne_to_sw(data_frame)
+                                       ]))
+    print "the largest overall product is %d" % largest_product
+
+
+def get_vert(data_frame):
     largest_product = 0
     for i in data_frame.index:
         s = Series(data_frame.ix[i, :])
         local_product = get_largest_product(s)
         if local_product > largest_product:
             largest_product = local_product
+    return largest_product
+
+
+def get_horiz(data_frame):
+    largest_product = 0
     for i in data_frame.columns:
         s = Series(data_frame.ix[:, i])
         local_product = get_largest_product(s)
         if local_product > largest_product:
             largest_product = local_product
-    print "the largest overall product is %d" % largest_product
-    # next test the diagonals
+    return largest_product
+
+
+def get_nw_to_se(data_frame):
+    largest_product = 0
+    (x, y) = np.shape(data_frame)
+    for j in range(19):
+        k = 0
+        lst = []
+        lst.append(data_frame.ix[j, k])
+        while j > 0:
+            j -= 1
+            k += 1
+            lst.append(data_frame.ix[j, k])
+        s = Series(lst)
+        local_product = get_largest_product(s)
+        if local_product > largest_product:
+            largest_product = local_product
+    return largest_product
+
+
+def get_se_to_nw(data_frame):
+    largest_product = 0
+    (x, y) = np.shape(data_frame)
+    for j in reversed(range(19)):
+        k = 19
+        lst = []
+        lst.append(data_frame.ix[j, k])
+        while j < 19:
+            j += 1
+            k -= 1
+            lst.append(data_frame.ix[j, k])
+        s = Series(lst)
+        local_product = get_largest_product(s)
+        if local_product > largest_product:
+            largest_product = local_product
+    return largest_product
+
+
+def get_sw_to_ne(data_frame):
+    largest_product = 0
+    (x, y) = np.shape(data_frame)
+    for j in reversed(range(19)):
+        k = 0
+        lst = []
+        lst.append(data_frame.ix[j, k])
+        while j < 19:
+            j += 1
+            k += 1
+            lst.append(data_frame.ix[j, k])
+        s = Series(lst)
+        local_product = get_largest_product(s)
+        if local_product > largest_product:
+            largest_product = local_product
+    return largest_product
+
+
+def get_ne_to_sw(data_frame):
+    largest_product = 0
+    (x, y) = np.shape(data_frame)
+    for j in range(19):
+        k = 19
+        lst = []
+        lst.append(data_frame.ix[j, k])
+        while j > 0:
+            j -= 1
+            k -= 1
+            lst.append(data_frame.ix[j, k])
+        s = Series(lst)
+        local_product = get_largest_product(s)
+        if local_product > largest_product:
+            largest_product = local_product
     return largest_product
 
 
 # do just a generic Series
 def get_largest_product(ser):
     maxlen = 4 if (ser.size > 3) else ser.size
-    print "max length = %d" % maxlen
     largest_product = 0
     for i in ser.index:
         local_product = ser[i]
-        print "i, local_product = %d, %d" % (i, local_product)
         if i + maxlen > ser.size:
-            print "max len extends beyond the end of the series"
             maxlen = ser.size - i
-            print " new max length = %d" % maxlen
         for j in ser[i + 1: i + maxlen]:
             local_product *= j
-            print " j, local_product = %d, %d" % (j, local_product)
         if local_product > largest_product:
             largest_product = local_product
-            print "new largest product = %d" % largest_product
     return largest_product
 
 
-
-# mylist = []
-# mylist.append(df.ix[0,0])
-# mylist.append(df.ix[1,1])
-# mylist.append(df.ix[2,2])
-# arr0 = np.array(mylist)
-# or
-# ser0 = Series(mylist)
-# s = Series(df.ix[:,0])
+if __name__ == '__main__':
+    scan(get_the_data())
