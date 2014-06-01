1a50744b6a3647c765e68b9896a663807288fe87
Gabriel Perdue <gnperdue@me.com>
2014-06-01 12:56:26 -0500

    Better Python file handling.

--- Code/Python/problem013/euler013.py
+++ Code/Python/problem013/euler013.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-the_file = open("nums.txt", "r")
 the_sum = 0
-for line in the_file:
-    num = int(line)
-    the_sum += num
+with open("nums.txt", "r") as the_file:
+    for line in the_file:
+        num = int(line)
+        the_sum += num
+the_file.close()
 
 print str(the_sum)[:10]
