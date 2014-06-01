da72ebe3d1ff32851b57f01cd31629f918651b3f
Gabriel Perdue <gnperdue@me.com>
2014-06-01 13:32:39 -0500

    Do problem 19 with pandas date range functions in Python.

---
+++ Code/Python/problem019/euler019.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+if __name__ == '__main__':
+    count = 0
+    index = pd.date_range('1/1/1901', '12/31/2000', freq='WOM-1SUN')
+    for i in index:
+        if i.day == 1:
+            count += 1
+
+    print "The number of months start on a Sunday in the 20th Century was:"
+    print count
