0bb1c1f8f3fc092f5a7294bde0623a55e45f36d5
Gabriel Perdue <perdue@fnal.gov>
2016-03-24 16:24:03 -0500

    better python2-3, make some simple optimizations

--- Code/Python/problem005/euler005.py
+++ Code/Python/problem005/euler005.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+from __future__ import print_function
 import numpy as np
 
-
-def is_evenly_div_by_all(testnum, numlist):
-    return 0 == np.mod(testnum, numlist).sum()
+testList = np.array([7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 
 
 def euler(testnum):
-    testList = np.array([7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
-    while not is_evenly_div_by_all(testnum, testList):
+    while any(np.mod(testnum, testList)):
         testnum += 1
     return testnum
 
