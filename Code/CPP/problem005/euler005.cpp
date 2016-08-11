4f0f8d264264dbe6712997ca703c5899cbfb5fe1
Gabriel Perdue <gnperdue@me.com>
2016-08-11 08:44:49 -0500

    linting

--- Code/CPP/problem005/euler005.cpp
+++ Code/CPP/problem005/euler005.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
+#include <iostream>    // NOLINT(readability/streams)
 #include <vector>
 
 bool evenDivisByAll(int num, const std::vector<int> &divs);
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   int evenlyDivisible{1};
-  std::vector<int> xs{7,11,12,13,14,15,16,17,18,19,20};
+  std::vector<int> xs{7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
   for (;;) {
-    if (evenDivisByAll(evenlyDivisible,xs)) {
-      std::cout << evenlyDivisible << 
+    if (evenDivisByAll(evenlyDivisible, xs)) {
+      std::cout << evenlyDivisible <<
         " is evenly divisible by all the numbers.\n";
       break;
     }
@@ -20,8 +19,7 @@ int main(int argc, char * argv[])
   return 0;
 }
 
-bool evenDivisByAll(int num, const std::vector<int> &divs)
-{
+bool evenDivisByAll(int num, const std::vector<int> &divs) {
   bool isEvenDivisByAll = true;
   for (const auto x : divs) {
     if (0 != num % x) {
--- Code/CPP/problem005/euler005_tmplt.cpp
+++ Code/CPP/problem005/euler005_tmplt.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
+#include <iostream>    // NOLINT(readability/streams)
 #include <vector>
 
 template<typename N>
-bool evenDivisByAll(N num, const std::vector<N> &divs)
-{
+bool evenDivisByAll(N num, const std::vector<N> &divs) {
   bool isEvenDivisByAll = true;
   for (const auto x : divs) {
     if (0 != num % x) {
@@ -14,15 +13,14 @@ bool evenDivisByAll(N num, const std::vector<N> &divs)
   return isEvenDivisByAll;
 }
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   int evenlyDivisible{1};
   std::vector<int> xs;
-  for (int i=1; i<=20; ++i) xs.push_back(i);
+  for (int i = 1; i <= 20; ++i) xs.push_back(i);
 
   for (;;) {
-    if (evenDivisByAll(evenlyDivisible,xs)) {
-      std::cout << evenlyDivisible << 
+    if (evenDivisByAll(evenlyDivisible, xs)) {
+      std::cout << evenlyDivisible <<
         " is evenly divisible by all the numbers.\n";
       break;
     }
