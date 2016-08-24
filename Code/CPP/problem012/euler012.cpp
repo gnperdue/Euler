4117410381c1150d74c118810dabf493f93910cc
Gabriel Perdue <gnperdue@me.com>
2016-08-24 17:43:29 -0500

    problem 12 in C++ (make the linter happy)

--- Code/CPP/problem012/euler012.cpp
+++ Code/CPP/problem012/euler012.cpp
@@ -1,20 +1,19 @@
-#include <iostream>
+#include <iostream>   // NOLINT(readability/streams)
 
-#include "TriangularNumber.h"
+#include "TriangularNumber.h"   // NOLINT(build/include)
 
-int main(int argc, char * argv[]) 
-{
+int main(int argc, char * argv[]) {
   using std::cout;
   using std::endl;
 
-	unsigned long maxfacts = 500L;
-  unsigned long currentMax = 0L;
+	uint64_t maxfacts = 500L;
+  uint64_t currentMax = 0L;
 
   TriangularNumber triangleCalc;
-	unsigned long index = 1L;
+	uint64_t index = 1L;
   triangleCalc.set(index);
-	std::vector<unsigned long> facts = triangleCalc.factors();
-	unsigned long nfacts = facts.size();
+	std::vector<uint64_t> facts = triangleCalc.factors();
+	uint64_t nfacts = facts.size();
 
 	while (nfacts <= maxfacts) {
 		triangleCalc.set(++index);
@@ -22,11 +21,11 @@ int main(int argc, char * argv[])
 		nfacts = facts.size();
     if (nfacts > currentMax) {
       currentMax = nfacts;
-      cout << "The first trianglar number with more than " << currentMax 
+      cout << "The first trianglar number with more than " << currentMax
         << " factors is " << triangleCalc.get() << endl;
     }
   }
-  cout << "The first trianglar number with more than " << maxfacts 
+  cout << "The first trianglar number with more than " << maxfacts
     << " factors is " << triangleCalc.get() << endl;
   cout << "  The factors are: " << endl;
   for (auto x : facts) cout << " " << x;
