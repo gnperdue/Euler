ed25e9babc8e8171ffa8b2f214085d618f1598f8
Gabriel Perdue <gnperdue@me.com>
2016-08-29 17:43:11 -0500

    linter stuff

--- Code/CPP/problem014/euler014.cpp
+++ Code/CPP/problem014/euler014.cpp
@@ -1,108 +1,101 @@
-#include <iostream>
+#include <iostream>  // NOLINT(readability/streams)
 #include <iomanip>
 #include <list>
 #include <thread>
 #include <mutex>
-#include "Collatz.h"
+#include <utility>
+#include "Collatz.h"  // NOLINT(build/include)
 
-void fillList( std::list< std::pair<unsigned long, unsigned int> >& collatzLengths, 
-    unsigned long lowerBound, unsigned long upperBound);
+void fillList(std::list< std::pair<uint64_t, unsigned int> >& collatzLengths,
+    uint64_t lowerBound, uint64_t upperBound);
 
-void addToFullListOfSeqs( 
-    std::list< std::pair<unsigned long, unsigned int> >& collatzLengths,
-    unsigned long init, unsigned int length );
+void addToFullListOfSeqs(
+    std::list< std::pair<uint64_t, unsigned int> >& collatzLengths,
+    uint64_t init, unsigned int length);
 
-std::list< std::pair<unsigned long, unsigned int> > collatzLengths;
+std::list<std::pair<uint64_t, unsigned int>> collatzLengths;
 
 std::mutex theMutex;
 
-typedef std::pair<unsigned long, unsigned int> colpair;
+typedef std::pair<uint64_t, unsigned int> colpair;
 
-int main(int argc, char * argv[]) 
-{
-  using namespace std;
+int main(int argc, char * argv[]) {
+  using std::cout;
+  using std::endl;
 
   collatzLengths.clear();
-  unsigned long maxValue = 100L;
-  if (argc > 1) maxValue = (unsigned long)(atoi(argv[1]) + 1);
-  unsigned long nDivisions = 4L;
-  if (argc > 2) nDivisions = (unsigned long)atoi(argv[2]);
+  uint64_t maxValue = 100L;
+  if (argc > 1) maxValue = (uint64_t)(atoi(argv[1]) + 1);
+  uint64_t nDivisions = 4L;
+  if (argc > 2) nDivisions = (uint64_t)atoi(argv[2]);
 
-  unsigned long increment = maxValue / nDivisions;
-  unsigned long lowerBound = 1L;
-  unsigned long upperBound = lowerBound + increment;
+  uint64_t increment = maxValue / nDivisions;
+  uint64_t lowerBound = 1L;
+  uint64_t upperBound = lowerBound + increment;
 
-  int nBlocks = (int)nDivisions;  
+  int nBlocks = static_cast<int>(nDivisions);
   cout << "Using " << nBlocks << " threads / computation blocks." << endl;
-  vector<thread> threads(nBlocks);
+  std::vector<std::thread> threads(nBlocks);  // NOLINT(build/include_what_you_use)
   int threadCounter = 0;
 
   while (upperBound < maxValue) {
-    cout << "Get Collatz seqs for range: [" << lowerBound 
+    cout << "Get Collatz seqs for range: [" << lowerBound
      << "," << upperBound << ")" << endl;
     cout << " Launching thread " << threadCounter << endl;
-    threads[threadCounter++] = 
-      thread( fillList, ref(collatzLengths), lowerBound, upperBound );
+    threads[threadCounter++] =
+      std::thread(fillList, ref(collatzLengths), lowerBound, upperBound);
     lowerBound = upperBound;
     upperBound += increment;
   }
-  if ( upperBound < maxValue ) {
-    cout << "Get Collatz seqs for range: [" << upperBound 
+  if (upperBound < maxValue) {
+    cout << "Get Collatz seqs for range: [" << upperBound
      << "," << maxValue << ")" << endl;
     cout << " Launching thread " << threadCounter << endl;
-    threads[threadCounter++] = 
-      thread( fillList, ref(collatzLengths), upperBound, maxValue );
-  }
-  else if ( lowerBound < maxValue ) {
-    cout << "Get Collatz seqs for range: [" << lowerBound 
+    threads[threadCounter++] =
+      std::thread(fillList, ref(collatzLengths), upperBound, maxValue);
+  } else if (lowerBound < maxValue) {
+    cout << "Get Collatz seqs for range: [" << lowerBound
      << "," << maxValue << ")" << endl;
     cout << " Launching thread " << threadCounter << endl;
-    threads[threadCounter++] = 
-      thread( fillList, ref(collatzLengths), lowerBound, maxValue );
+    threads[threadCounter++] =
+      std::thread(fillList, ref(collatzLengths), lowerBound, maxValue);
   }
 
   cout << "Now join " << threadCounter << " threads..." << endl;
-  for_each( threads.begin(), threads.end(),
-      mem_fn(&thread::join) );
+  for_each(threads.begin(), threads.end(),
+      mem_fn(&std::thread::join));
 
   cout << "Longest Chain in the List:" << endl;
   unsigned int maxLength = 0;
-  unsigned long maxLengthIndex = 0;
-  for (list< colpair >::iterator p=collatzLengths.begin();
-      p!=collatzLengths.end();
+  uint64_t maxLengthIndex = 0;
+  for (std::list<colpair>::iterator p = collatzLengths.begin();
+      p != collatzLengths.end();
       ++p) {
-    if ( (*p).second > maxLength ) {
+    if ((*p).second > maxLength) {
       maxLength = (*p).second;
       maxLengthIndex = (*p).first;
     }
   }
-  cout << "(init,length) = " 
-    << setw(7) << setfill(' ') << maxLengthIndex << "\t" 
-    << setw(7) << setfill(' ') << maxLength << endl;
+  cout << "(init,length) = "
+    << std::setw(7) << std::setfill(' ') << maxLengthIndex << "\t"
+    << std::setw(7) << std::setfill(' ') << maxLength << endl;
 
   return 0;
 }
 
-void fillList( std::list< colpair >& collatzLengths, 
-    unsigned long lowerBound, unsigned long upperBound)
-{
-  using namespace std;
-
-  Collatz *collatz = new Collatz();
-  for (unsigned long i = lowerBound; i < upperBound; ++i) {
-    collatz->reset( i );    
-    addToFullListOfSeqs( collatzLengths, i, collatz->size() );
+void fillList(std::list<colpair>& collatzLengths,
+    uint64_t lowerBound, uint64_t upperBound) {
+  Collatz collatz;
+  for (uint64_t i = lowerBound; i < upperBound; ++i) {
+    collatz.reset(i);
+    addToFullListOfSeqs(collatzLengths, i, collatz.size());
   }
-  delete collatz;
 }
 
-void addToFullListOfSeqs( 
-    std::list< colpair >& collatzLengths,
-    unsigned long init, unsigned int length )
-{
-  using namespace std;
-
-  lock_guard<mutex> guard(theMutex);
+void addToFullListOfSeqs(
+    std::list<colpair>& collatzLengths,
+    uint64_t init, unsigned int length) {
+  std::lock_guard<std::mutex> guard(theMutex);
   collatzLengths.push_back(colpair(init, length));
 }
 
