e06a587aa08485d7ed3e1a8b494498a8047926c1
Gabriel Perdue <gnperdue@me.com>
2016-10-04 18:01:56 -0500

    class to get the proper divisors of a number

---
+++ Code/CPP/euler/ProperDivisors.cpp
@@ -0,0 +1,40 @@
+#include "ProperDivisors.h"   // NOLINT(build/include)
+#include <cmath>
+#include <cstdlib>
+
+//-----------------------------------------------------------------------------
+ProperDivisors::ProperDivisors() : value(0) {}
+
+//-----------------------------------------------------------------------------
+ProperDivisors::ProperDivisors(uint64_t val) : value(val) {}
+
+//-----------------------------------------------------------------------------
+void ProperDivisors::set(uint64_t val) {
+  value = val;
+}
+
+//-----------------------------------------------------------------------------
+std::vector<uint64_t> ProperDivisors::divisors() const {
+  std::vector<uint64_t> divs = {1};  // NOLINT(build/include_what_you_use)
+  uint64_t ubound = sqrt(value);
+  for (uint64_t x = 2; x < ubound; ++x) {
+    // get quotient and remainder in one step - actually optimized?
+    std::lldiv_t result = lldiv(value, x);
+    if (result.rem == 0) {
+      divs.push_back(x);
+      divs.push_back(result.quot);
+    }
+  }
+  return divs;
+}
+
+//-----------------------------------------------------------------------------
+uint64_t ProperDivisors::sum_of_divisors() const {
+  std::vector<uint64_t> divs = divisors();  // NOLINT(build/include_what_you_use)
+  uint64_t sum = 0;
+  for (auto i : divs) {
+    sum += i;
+  }
+  return sum;
+}
+
