19b49474c9978f84ab4856faeab1d33992525663
Gabriel Perdue <gnperdue@me.com>
2016-10-28 18:34:40 -0500

    bug fix - needed to ceil the sqrt

--- Code/CPP/euler/ProperDivisors.cpp
+++ Code/CPP/euler/ProperDivisors.cpp
@@ -16,7 +16,7 @@ void ProperDivisors::set(uint64_t val) {
 //-----------------------------------------------------------------------------
 std::vector<uint64_t> ProperDivisors::divisors() const {
   std::vector<uint64_t> divs = {1};  // NOLINT(build/include_what_you_use)
-  uint64_t ubound = sqrt(value);
+  uint64_t ubound = ceil(sqrt(value));
   for (uint64_t x = 2; x < ubound; ++x) {
     // get quotient and remainder in one step - actually optimized?
     std::lldiv_t result = lldiv(value, x);
