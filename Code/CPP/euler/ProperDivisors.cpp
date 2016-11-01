2ca4d59303374072f3815f91412a0e2535500020
Gabriel Perdue <gnperdue@me.com>
2016-11-01 17:23:45 -0500

    bug fix - error when getting divisors for nums that are the squares of integers

--- Code/CPP/euler/ProperDivisors.cpp
+++ Code/CPP/euler/ProperDivisors.cpp
@@ -17,12 +17,14 @@ void ProperDivisors::set(uint64_t val) {
 std::vector<uint64_t> ProperDivisors::divisors() const {
   std::vector<uint64_t> divs = {1};  // NOLINT(build/include_what_you_use)
   uint64_t ubound = ceil(sqrt(value));
-  for (uint64_t x = 2; x < ubound; ++x) {
+  for (uint64_t x = 2; x <= ubound; ++x) {
     // get quotient and remainder in one step - actually optimized?
     std::lldiv_t result = lldiv(value, x);
     if (result.rem == 0) {
       divs.push_back(x);
-      divs.push_back(result.quot);
+      if (x != result.quot) {
+        divs.push_back(result.quot);
+      }
     }
   }
   return divs;
