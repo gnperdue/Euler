75a58bc85ec21d6c828eafdedf517fd8dc5b9c6b
Gabriel Perdue <gnperdue@me.com>
2016-11-01 17:25:24 -0500

    indentation

--- Code/CPP/problem022/euler022.cpp
+++ Code/CPP/problem022/euler022.cpp
@@ -18,26 +18,26 @@ std::vector<std::string> split_by_comma(
     const std::string &line, bool debug = false
     ) {
   const char sep = static_cast<char>(44);
-	std::vector<std::string> tokens;
-	std::size_t start = 0, end = 0;
-	while ((end = line.find(sep, start)) != std::string::npos) {
-		if (end != start) {
-			if (debug) {
-				std::cout << "start,end = " << start << "," << end << ": " <<
-					clean_quotes(line.substr(start, end - start)) << std::endl;
-			}
-			tokens.push_back(clean_quotes(line.substr(start, end - start)).c_str());
-		}
-		start = end + 1;
-	}
-	if (end != start) {
-		if (debug) {
-			std::cout << "start,end = " << start << "," << end << ": " <<
-				clean_quotes(line.substr(start, end - start)) << std::endl;
-		}
-		tokens.push_back(clean_quotes(line.substr(start).c_str()));
-	}
-	return tokens;
+  std::vector<std::string> tokens;
+  std::size_t start = 0, end = 0;
+  while ((end = line.find(sep, start)) != std::string::npos) {
+    if (end != start) {
+      if (debug) {
+        std::cout << "start,end = " << start << "," << end << ": " <<
+          clean_quotes(line.substr(start, end - start)) << std::endl;
+      }
+      tokens.push_back(clean_quotes(line.substr(start, end - start)).c_str());
+    }
+    start = end + 1;
+  }
+  if (end != start) {
+    if (debug) {
+      std::cout << "start,end = " << start << "," << end << ": " <<
+        clean_quotes(line.substr(start, end - start)) << std::endl;
+    }
+    tokens.push_back(clean_quotes(line.substr(start).c_str()));
+  }
+  return tokens;
 }
 
 // score the name
@@ -52,33 +52,33 @@ uint64_t score_name(std::string& name, uint64_t index) {
 
 int main(int argc, char * argv[]) {
 
-	std::string file_name = "p022_names_short.txt";
+  std::string file_name = "p022_names_short.txt";
   bool print_names = false;
 
-	int optind = 1;
-	while ((optind < argc) && argv[optind][0] == '-') {
-		std::string sw = argv[optind];
-		if (sw == "-f" || sw == "--file") {
-			optind++;
-			file_name = argv[optind];
-		}
-		if (sw == "-p" || sw == "--print_names") {
-			print_names = true;
-		}
-		optind++;
-	}
+  int optind = 1;
+  while ((optind < argc) && argv[optind][0] == '-') {
+    std::string sw = argv[optind];
+    if (sw == "-f" || sw == "--file") {
+      optind++;
+      file_name = argv[optind];
+    }
+    if (sw == "-p" || sw == "--print_names") {
+      print_names = true;
+    }
+    optind++;
+  }
 
   std::vector<std::string> names;
-	std::string line;
+  std::string line;
 
   // open the file
-	std::ifstream file(file_name);
+  std::ifstream file(file_name);
   // read the file in line by line
-	while (std::getline(file, line)) {
-		std::vector<std::string> linevals = split_by_comma(line);
+  while (std::getline(file, line)) {
+    std::vector<std::string> linevals = split_by_comma(line);
     // extend our vector of tokens (names)
-		names.insert(names.end(), linevals.begin(), linevals.end());
-	}
+    names.insert(names.end(), linevals.begin(), linevals.end());
+  }
 
   if (print_names) {
     std::cout << "\nNames...\n" << std::endl;
