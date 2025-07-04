62d58e94138edab1cb8d9a778319ac3255a4130d
Gabriel Perdue <gnperdue@hey.com>
2025-07-04 16:07:54 -0500

    futz with problem001

--- Code/Julia/problem001/euler001.jl
+++ Code/Julia/problem001/euler001.jl
@@ -1,6 +1,6 @@
 sum = 0
-for i in 1:1000
-    if i%3 == 0 || i%5 == 0
+for i in 1:999
+    if i % 3 == 0 || i % 5 == 0
         global sum += i
     end
 end
---
+++ Code/Julia/problem001/euler001_2.jl
@@ -0,0 +1,1 @@
+println(sum(i for i in 1:999 if i % 3 == 0 || i % 5 == 0))
